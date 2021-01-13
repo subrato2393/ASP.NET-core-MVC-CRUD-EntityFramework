b0ea0e4e73ab61e0171978276553bc228edc0091
Subrato Ghosh <subrato2393@gmail.com>
2021-01-13 19:45:51 +0600

    modify gitugnore file

---
+++ CRUDAppEFCore/CRUDAppEFCore/Models/StudentModel.cs
@@ -0,0 +1,42 @@
+﻿using Autofac;
+using CRUDAppEFCore.Context;
+using CRUDAppEFCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUDAppEFCore.Models
+{
+    public class StudentModel
+    { 
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public int DepartmentId { get; set; } 
+        public virtual DepartmentModel Department { get; set; }
+        private readonly CrudDbContext _db;
+        //public string DepartmentName { get; set; }
+        //public Student()
+        //{
+        //    var student = Startup.AutofacContainer.Resolve<Student>();
+        //}
+        public StudentModel()
+        {
+            _db = Startup.AutofacContainer.Resolve<CrudDbContext>();
+        }
+        public void Add(StudentModel student)
+        {
+            _db.Students.Add(student);
+        }
+        public void SaveChanges()
+        {
+            _db.SaveChanges();
+        }
+        public IList<DepartmentModel> GetDepartments()
+        {
+            return _db.Departments.ToList();
+        }
+    }
+}
---
+++ CRUDAppEFCore/CRUDAppEFCore/WebModule.cs
@@ -0,0 +1,30 @@
+﻿using Autofac;
+using CRUDAppEFCore.Context;
+using CRUDAppEFCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUDAppEFCore
+{
+    public class WebModule:Module
+    {
+        private readonly string _connectionString;
+        private readonly string _migrationAssemblyName;
+
+        public WebModule(string connectionString, string migrationAssemblyName)
+        {
+            _connectionString = connectionString;
+            _migrationAssemblyName = migrationAssemblyName;
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<CrudDbContext>()
+           .WithParameter("connectionString", _connectionString).AsSelf();
+
+            builder.RegisterType<StudentModel>().AsSelf();
+        }
+    }
+}
